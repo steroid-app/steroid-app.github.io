f6d701656a45e95f1523a23e0e58d7c7316827c2
N0XT <gabrielatmoon666@hotmail.com>
2021-10-31 16:59:14 -0300

    recovery patch

--- js/recovery.js
+++ js/recovery.js
@@ -1,8 +1,8 @@
 window.onload = function(){
 
     const url = window.location.search;
-    const ticket = url.split('?ticket=').pop();
-    ticket == "" ? window.location.replace("/") : false;
+    let ticket = url.split('?ticket=').pop();
+    //ticket == "" ? window.location.replace("/") : false;
 
     const steroid = {
         // CORE VARIABLES
@@ -17,7 +17,6 @@ window.onload = function(){
                 headers: steroid.header,
                 body: "user_id="+user_id+"&password="+password+"&ticket="+ticket,
             }).then(res => {
-                console.log(res);
                 switch(res.status){
                     case 200: response = {success: "Your password has been changed."}; break;
                     case 401: response = {error: "Email invalid, try again.", code: 401}; break;
@@ -32,7 +31,6 @@ window.onload = function(){
     const emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     //Nav bar
-    const mobileButton = document.getElementById("navbar-burger");
     const nav = document.getElementById("navbarHome");
 
 
@@ -51,10 +49,6 @@ window.onload = function(){
     const notificationText = document.getElementById("notification-text");
 
     // Event Listener Assignament
-    mobileButton.addEventListener("click", function(){
-        nav.classList.toggle("is-active");
-    });
-
     recoveryForm.addEventListener("submit", function(event){
         event.preventDefault();
         checkRecoverySubmit();
@@ -65,7 +59,7 @@ window.onload = function(){
         document.getElementsByTagName("body")[0].style.overflow = "";
     });
 
-    async function checkRecoverySubmit(event) {
+    async function checkRecoverySubmit(event){
         if (emailPattern.test(emailRecovery.value)){
             document.getElementById("email-recovery-check").classList.remove("is-hidden");
             emailRecovery.classList.remove("is-danger");
@@ -86,13 +80,10 @@ window.onload = function(){
                     let response = await steroid.recovery(emailRecovery.value, passwordRecovery.value, ticket);
                     if (response.error !== undefined){
                         document.getElementById("email-recovery-check").classList.add("is-hidden");
-                        emailRecovery.classList.remove("is-success");
-                        emailRecovery.classList.add("is-danger");
-                        document.getElementById("email-recovery-warning").classList.remove("is-hidden");
                         document.getElementById("submit-recovery-button").classList.remove("is-loading");
                         document.getElementById("submit-recovery-button").classList.add("is-danger");
                         document.getElementsByTagName("body")[0].style.pointerEvents = "auto";
-                        response.code !== 401 ? displayNotification(response) : false;
+                        displayNotification(response);
                     } else {
                         document.getElementsByTagName("body")[0].style.pointerEvents = "auto";
                         document.getElementById("submit-recovery-button").classList.remove("is-loading");
